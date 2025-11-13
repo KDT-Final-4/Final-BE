66ded9a4245dd0e9c3691794436a124c25dd27f6
MiSa-JD <jeongs0427@gmail.com>
2025-11-13 18:39:10 +0900

    initial commit

---
+++ src/test/java/com/final_team4/finalbe/FinalFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_team4.finalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalFeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
