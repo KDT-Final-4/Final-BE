fea18227679a7c08f47934ce273f7f7a36e8eaea
Logan <uhee0112@gmail.com>
2025-11-20 14:26:38 +0900

    refactor: 필요없는 필드 삭제

--- src/main/java/com/final_team4/finalbe/schedule/dto/ScheduleUpdateRequestDto.java
+++ src/main/java/com/final_team4/finalbe/schedule/dto/ScheduleUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.final_team4.finalbe.schedule.dto;
 
+import com.final_team4.finalbe.schedule.domain.Schedule;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,4 @@ public class ScheduleUpdateRequestDto {
     private String title;
     private LocalDateTime startTime;
     private String repeatInterval;
-    private LocalDateTime lastExecutedAt;
-
 }
