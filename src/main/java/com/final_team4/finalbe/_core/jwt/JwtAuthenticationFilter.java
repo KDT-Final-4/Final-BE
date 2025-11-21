200c22de8c6c4bcf52642e3f26934c7b103a6a2c
Logan <uhee0112@gmail.com>
2025-11-21 11:09:59 +0900

    refactor: bean 이중 등록 현상 수정

--- src/main/java/com/final_team4/finalbe/_core/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/final_team4/finalbe/_core/jwt/JwtAuthenticationFilter.java
@@ -10,11 +10,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private static final String BEARER_PREFIX = "Bearer ";
