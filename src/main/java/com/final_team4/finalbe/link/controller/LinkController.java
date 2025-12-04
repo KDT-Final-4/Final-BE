eb9db146917debc1706c1c0ccfdc76ebe2870d8d
finspp <cbmcsm@gmail.com>
2025-12-04 17:11:05 +0900

    refactor : link 패키지 스웨거 문서 추가

--- src/main/java/com/final_team4/finalbe/link/controller/LinkController.java
+++ src/main/java/com/final_team4/finalbe/link/controller/LinkController.java
@@ -2,6 +2,10 @@ package com.final_team4.finalbe.link.controller;
 
 import com.final_team4.finalbe.link.dto.LinkResponseDto;
 import com.final_team4.finalbe.link.service.LinkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +18,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/link")
 @RequiredArgsConstructor
 @Validated
+@Tag(name= "Link", description = "링크 조회 API")
 public class LinkController {
 
     private final LinkService linkService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(
+            summary = "jobId로 링크 조회",
+            description = "jobId를 받아 사용자의 IP와 함께 링크 정보를 반환합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "400", description = "jobId 누락 또는 잘못된 요청")
+    })
     public LinkResponseDto getLink(@RequestParam("jobId") @NotBlank String jobId, HttpServletRequest request) {
         String clientIp = resolveClientIp(request);
         return linkService.resolveLink(jobId,clientIp);
